f98e3836d38509c63fe817a9cd8dc0cf1c2152ed
vladimirsachkov <50697874+vladimirsachkov@users.noreply.github.com>
2023-05-16 23:28:38 +0300

    Add files via upload

---
+++ js/send.js
@@ -0,0 +1,7 @@
+function sendToEmail() {
+    var secondName = document.getElementById('secondname').value;
+    var firstname = document.getElementById('firstname').value;
+    var thirdname = document.getElementById('thirdname').value;
+
+    alert(secondName + ' ' + firstname + ' ' + thirdname + ', ваше обращение отправлено! Ожидайте ответа.');
+}
